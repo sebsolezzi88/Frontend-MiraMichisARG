32cf8c732e8f8cf457489124ba383f49cb6957b5
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-20 18:59:55 -0300

    Creacion de  la funcion loginUser

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
-import type { ApiResponse, RegisterData } from "../types/types";
+import type { ApiResponse, LoginApiResponse, RegisterData } from "../types/types";
 
 const URL:string = import.meta.env.VITE_API_URL;
 
 export const registerUser = async (registerData:RegisterData) =>{
     const res = await axios.post<ApiResponse>(`${URL}/user/register`, registerData);
+    return res.data;
+}
+
+export const loginUser = async (loginData:RegisterData) =>{
+    const res = await axios.post<LoginApiResponse>(`${URL}/user/login`, loginData);
     return res.data;
 }
