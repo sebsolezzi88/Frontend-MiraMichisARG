db5d558db0e1dc882f95deeaa952709f50b7a3f9
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-20 11:51:48 -0300

    Agregando typo a la api register

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import type { RegisterData } from "../types/types";
+import type { ApiResponse, RegisterData } from "../types/types";
 
 const URL:string = import.meta.env.VITE_API_URL;
 
 export const registerUser = async (registerData:RegisterData) =>{
-    const res = await axios.post(`${URL}/user/register`, registerData);
+    const res = await axios.post<ApiResponse>(`${URL}/user/register`, registerData);
     return res.data;
 }
