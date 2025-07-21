7daaa184c87e8b5820bbde0d6089d828e7e0f06e
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-20 22:15:24 -0300

    Creacion del form para resetear password

---
+++ src/page/ResetPassword.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+
+const ResetPassword = () => {
+
+    const [formData, setFormData] = useState({password:'',passwordrep:'',token:''});
+
+  return (
+    <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8 space-y-6">
+            <div className="text-center">
+                <h2 className="text-2xl font-extrabold text-gray-800 mb-2">
+                    ¿No recuerdas tu contraseña?
+                </h2>
+                <p className="text-gray-600">
+                    Ingresa tu email para resetear tu password.
+                </p>
+            </div>
+
+            <form  className="space-y-4">
+                <div>
+                    <label htmlFor="username" className="block text-sm font-medium text-gray-700 mb-1">password</label>
+                    <input
+                        onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})}
+                        value={formData.password}
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Tu nombre de usuario"
+                        className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm 
+                           focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="username" className="block text-sm font-medium text-gray-700 mb-1">password</label>
+                    <input
+                        onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})}
+                        value={formData.passwordrep}
+                        type="password"
+                        id="passwordrep"
+                        name="passwordrep"
+                        placeholder="Tu nombre de usuario"
+                        className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm 
+                           focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
+                        required
+                    />
+                </div>
+
+
+                <div>
+                    <button
+                        type="submit"
+                        className="w-full flex justify-center py-2 px-4 border border-transparent 
+                           rounded-md shadow-sm text-sm font-medium text-white 
+                           bg-orange-500 hover:bg-orange-600 focus:outline-none 
+                           focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 
+                           transition duration-300"
+                    >
+                        Reestrablecer
+                    </button>
+                </div>
+            </form>
+
+        
+        </div>
+  )
+}
+
+export default ResetPassword
