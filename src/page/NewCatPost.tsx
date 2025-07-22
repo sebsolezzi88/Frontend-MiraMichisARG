41ab190a60ff9015284f50ddf4d2498f1e0d9cd9
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-22 11:25:01 -0300

    Probando el submit

--- src/page/NewCatPost.tsx
+++ src/page/NewCatPost.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent } from "react"
+import { useState, type ChangeEvent, type FormEvent } from "react"
 import type { CatPostFormData } from "../types/types"
 
 const NewCatPost = () => {
@@ -14,6 +14,13 @@ const NewCatPost = () => {
             province: '',
             photo: null,
     });
+
+    //submit del formulario
+
+    const handletSubmit = async (e:FormEvent) =>{
+        e.preventDefault();
+        console.log(formData)
+    }
   return (
     <div className="bg-amber-50 min-h-screen flex items-center justify-center p-4">
 
@@ -27,7 +34,7 @@ const NewCatPost = () => {
             </p>
         </div>
 
-        <form action="#" method="POST" encType="multipart/form-data" className="space-y-4">
+        <form onSubmit={handletSubmit} encType="multipart/form-data" className="space-y-4">
             <div>
                 <label htmlFor="typeOfPublication" className="block text-sm font-medium text-gray-700 mb-1">Tipo de Publicación</label>
                 <select
@@ -120,13 +127,13 @@ const NewCatPost = () => {
             </div>
 
              <div>
-                <label htmlFor="ciudad" className="block text-sm font-medium text-gray-700 mb-1">Ciudad</label>
+                <label htmlFor="city" className="block text-sm font-medium text-gray-700 mb-1">Ciudad</label>
                 <input
                 onChange={(e:ChangeEvent<HTMLInputElement>)=>{setFormData({...formData, [e.target.name]:e.target.value})}}
                 value={formData.city}
                     type="text"
-                    id="ciudad"
-                    name="ciudad"
+                    id="city"
+                    name="city"
                     placeholder="Ej. San Nicolás de los Arroyos"
                     className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm
                            focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
