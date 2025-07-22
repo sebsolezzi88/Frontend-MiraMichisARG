dd382170d948484148d621fdbb581507bcc31038
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-21 22:02:12 -0300

    Cambios en el main

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -2,7 +2,16 @@
 
 const MainPage = () => {
   return (
-    <div>MainPage</div>
+    <div className="max-w-7xl mx-auto space-y-10">
+      <section className="bg-orange-100 rounded-xl shadow-md p-8 text-center">
+        <h2 className="text-4xl font-extrabold text-orange-800 mb-4">
+          ¡Bienvenido a MiraMichis ARG!
+        </h2>
+        <p className="text-lg text-orange-700 max-w-2xl mx-auto">
+          Tu comunidad para encontrar, adoptar y ayudar a michis en <span className="font-extrabold">Argentina</span>.
+        </p>
+      </section>
+    </div>
   )
 }
 
