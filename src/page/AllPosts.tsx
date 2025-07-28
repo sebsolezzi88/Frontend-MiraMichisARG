63fc2f6e4bb47ff792b30737fbc9237651883215
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-28 10:12:58 -0300

    Funcionalidad para cargar los catpost y redirigir si no esta el parametro

--- src/page/AllPosts.tsx
+++ src/page/AllPosts.tsx
@@ -1,27 +1,35 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import type { CatPost } from "../types/types";
-import { useParams } from "react-router-dom";
+import type { CatPost, TypeOfPublication } from "../types/types";
+import { useNavigate, useParams } from "react-router-dom";
+import { getAllCatPostsByType } from "../api/catPost";
 
 const AllPosts = () => {
 
     const [catPosts, setCatPosts] = useState<CatPost[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>('');
+    const navigate = useNavigate();
 
-    const {type} = useParams() //obtener parametro de tipo de la URL
-
-
+    const {type} = useParams<{ type: TypeOfPublication }>() //obtener parametro de tipo de la URL
 
   
-    /* useEffect(() => {
+     useEffect(() => {
+        const validTypes: TypeOfPublication[] = ["encontrado", "perdido", "adopción"];
+        if (!type || !validTypes.includes(type)) {
+            // Redireccionar si el tipo no es válido o está ausente
+            console.warn(`Tipo de publicación inválido o ausente: ${type}. Redireccionando...`);
+            navigate('/'); // Redireccionar al inicio
+            toast.error('La página solicitada no es válida.', { theme: "colored", autoClose: 3000 });
+            return; // Detiene la ejecución del resto del useEffect
+        }
         const getCatPots = async () => {
             try {
                 setLoading(true);
-                return
-                //const response = await getAllAdoptionCatPosts();
+                const response = await getAllCatPostsByType(type);
                 if (response.status === 'success' && response.posts) {
-                    
+                    setCatPosts(response.posts);
+                    console.log(catPosts)
                 } else {
                     toast.error('Hubo un problema al obtener Post de adopción', { theme: "colored", autoClose: 3000 });
                     setError('No se pudieron obtener los Post de adopción');
@@ -35,7 +43,7 @@ const AllPosts = () => {
             }
         }
         getCatPots();
-    }, []) */
+    }, []) 
 
     if (loading) return <div>Cargado...</div>
 
