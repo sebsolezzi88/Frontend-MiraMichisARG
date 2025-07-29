ed934f8e9d2434a718e6e1f31a192bcc2a1bb23a
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-29 10:47:07 -0300

    Creacion de funcion addCommentToCatPost para llamar a la api

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { ApiCommentResponse } from "../types/types";
+import type { ApiCommentResponse, ApiPostCommentResponse, PostCommentFormData } from "../types/types";
 
 const URL:string = import.meta.env.VITE_API_URL;
 
@@ -19,6 +19,36 @@ export const getCatPosAndCommentstById = async (id:string) =>{
         if (axios.isAxiosError(error)) {
             console.error("Error al obtener el catPost", error.response?.data || error.message);
             throw new Error(error.response?.data?.message || "Error desconocido al obtener el catPost.");
+        } else {
+            console.error("Error inesperado:", error);
+            throw new Error("Ocurrió un error inesperado.");
+        }
+    }
+}
+
+export const addCommentToCatPost = async (commentFormData:PostCommentFormData, catPostId:string) =>{
+    const authToken = localStorage.getItem('authToken');
+
+     if (!authToken) {
+        console.error("No hay token de autenticación disponible. El usuario no está logueado.");
+        throw new Error("No autenticado."); 
+    }
+
+     const headers = {
+        'Content-Type': 'multipart/form-data', 
+        'Authorization': `Bearer ${authToken}` 
+    };
+
+    try {
+        // El primer argumento es la URL, el segundo son los datos, el tercero es el objeto de configuración.
+        const res = await axios.post<ApiPostCommentResponse>(`${URL}/catpost/${catPostId}/comments`, commentFormData, { headers } );
+        return res.data;
+
+    } catch (error) {
+        // Manejo de errores (por ejemplo, token inválido, servidor caído, etc.)
+        if (axios.isAxiosError(error)) {
+            console.error("Error al crear comentario:", error.response?.data || error.message);
+            throw new Error(error.response?.data?.message || "Error desconocido al crear comentario.");
         } else {
             console.error("Error inesperado:", error);
             throw new Error("Ocurrió un error inesperado.");
