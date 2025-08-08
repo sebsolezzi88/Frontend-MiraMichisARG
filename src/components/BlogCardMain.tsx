659b8b1126b7b583ab2039e0025a1c642aef13c7
sebsolezzi88 <ssolezzi1@gmail.com>
2025-08-08 11:02:57 -0300

    Cambio de diseño en la card

--- src/components/BlogCardMain.tsx
+++ src/components/BlogCardMain.tsx
@@ -1,5 +1,11 @@
 import type { BlogPost } from "../types/types"
 import { formatDate } from "../utils/utils"
+import catEdu from '../assets/gatosescuela.png';
+import catSal from '../assets/gatosalud.png';
+import catVid from '../assets/gatosvideos.png';
+import catEve from '../assets/gatosevento.png';
+import catNot from '../assets/gatonoticias.png';
+
 
 import { Link } from "react-router-dom"
 
@@ -15,53 +21,54 @@ const BlogCardMain = ({ post }: BlogCardProps) => {
     let typeTagBgClass = 'bg-orange-100'; // Color de etiqueta por defecto
     let typeTagTextColor = 'text-orange-700';
     let cardBodyBgClass = 'bg-gray-100';
+    let catImg = '';
 
     if (post.typeOfBlogPost === 'noticia') {
         cardBorderClass = 'border-blue-500';
         cardBodyBgClass = 'bg-blue-100';
         typeTagBgClass = 'bg-blue-500';
         typeTagTextColor = 'text-white';
+        catImg = catNot;
     } else if (post.typeOfBlogPost === 'evento') {
         cardBorderClass = 'border-pink-500';
         cardBodyBgClass = 'bg-pink-100';
         typeTagBgClass = 'bg-pink-500';
         typeTagTextColor = 'text-white';
+        catImg = catEve;
     } else if (post.typeOfBlogPost === 'salud') {
         cardBorderClass = 'border-green-500';
         cardBodyBgClass = 'bg-green-100';
         typeTagBgClass = 'bg-green-500';
         typeTagTextColor = 'text-white';
+        catImg = catSal;
     } else if (post.typeOfBlogPost === 'video' || post.typeOfBlogPost === 'educación') {
         cardBorderClass = 'border-purple-500';
         cardBodyBgClass = 'bg-purple-100';
         typeTagBgClass = 'bg-purple-500';
         typeTagTextColor = 'text-white';
+        catImg = catVid
     }
 
 
 
     return (
-        <div className={`${cardBodyBgClass} rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 overflow-hidden border-t-4 ${cardBorderClass}`}>
-
-            <div className="p-6">
-                <h3 className="text-2xl font-bold text-gray-800 mb-2 leading-tight">{post.title}</h3>
-
-                <p className="text-gray-500 text-sm mt-1">Creadog: {formatDate(post.createdAt)}</p>
-
-                <span className={`inline-block text-xs font-semibold rounded-full px-3 py-1 mt-4 ${typeTagBgClass} ${typeTagTextColor}`}>
-                    {post.typeOfBlogPost}
-                </span>
+        <div className={`flex rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 overflow-hidden border-t-4 ${cardBorderClass}`}>
+            <img className="w-48 h-full object-cover" src={catImg} alt="img" />
+            <div className="p-6 flex flex-col justify-between">
+                <div>
+                    <h3 className="text-xl font-bold text-gray-800 mb-1 leading-tight">{post.title}</h3>
+                    <p className="text-gray-500 text-sm mt-0.5">Creado: {formatDate(post.createdAt)}</p>
+                    <span className={`inline - block text-xs font-semibold rounded-full px-2 py-0.5 mt-2 ${typeTagBgClass} ${typeTagTextColor}`}>
+                        {post.typeOfBlogPost}
+                    </span>
+                </div>
+                <div className="border-t border-purple-200 pt-4">
+                    <Link to={"/"} className="inline-flex items-center text-orange-600 hover:underline font-semibold text-sm">
+                        Leer
+                    </Link>
+                </div>
             </div>
-
-
-            <div className="bg-gray-50 border-t border-purple-200 px-6 py-4 flex justify-end items-center space-x-3">
-                <Link to={`/`} className="inline-flex items-center text-orange-600 hover:underline font-semibold text-sm">
-                    Leer
-                </Link>
-
-            </div>
-
-        </div>
+        </div >
     )
 }
 
