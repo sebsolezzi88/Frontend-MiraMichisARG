c22ff7f6597c6c636294113e4c38df0c6c34c5f6
sebsolezzi88 <ssolezzi1@gmail.com>
2025-08-10 12:13:31 -0300

    Creacion de la funcion sendMessage

--- src/api/message.ts
+++ src/api/message.ts
@@ -1,4 +1,41 @@
 import axios from "axios";
+import type { ApiMessagePostResponse, PostCommentFormData } from "../types/types";
 
 
 const URL:string = import.meta.env.VITE_API_URL;
+
+/* Funcion para mandar un mensaje a un usuario */
+export const sendMessage = async (data:PostCommentFormData) =>{
+    const authToken = localStorage.getItem('authToken');
+
+     if (!authToken) {
+        console.error("No hay token de autenticación disponible. El usuario no está logueado.");
+        throw new Error("No autenticado."); 
+    }
+
+     const headers = {
+        'Content-Type': 'application/json', 
+        'Authorization': `Bearer ${authToken}` 
+    };
+
+    try {
+        
+        const res = await axios.post<ApiMessagePostResponse>(
+            `${URL}/message`, 
+            data,
+            { headers } 
+        );
+        
+        return res.data;
+
+    } catch (error) {
+        // Manejo de errores (por ejemplo, token inválido, servidor caído, etc.)
+        if (axios.isAxiosError(error)) {
+            console.error("Error al enviar el mensaje:", error.response?.data || error.message);
+            throw new Error(error.response?.data?.message || "Error desconocido al enviar el mensaje.");
+        } else {
+            console.error("Error inesperado:", error);
+            throw new Error("Ocurrió un error inesperado.");
+        }
+    }
+}
