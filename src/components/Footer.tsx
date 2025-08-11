215e9669fc9c47d7e1394340bcebfcb649a1c38b
seba <ssolezzi@gmail.com>
2025-08-10 23:35:34 -0300

    Cambio en texto de footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     
   return (
      <footer className="text-center bg-gray-100 text-gray-600 text-sm mt-10 p-4 border-t border-gray-200">
-            <p>&copy; {new Date().getFullYear().toString()} MichiLand. Todos los derechos reservados.</p>
+            <p>&copy; {new Date().getFullYear().toString()} MiraMichis ARG. Todos los derechos reservados.</p>
             <p>Hecho para todos los Michis en Argentina</p>
     </footer>
   )
